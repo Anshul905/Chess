15e7faff73a551e61e80b75edf2ef97df6ab4df9
Anshul Kumar Neekhara <anshulneekhara@gmail.com>
2024-06-25 21:58:58 +0530

    Initialization done, server created & connected

--- app.js
+++ app.js
@@ -2,3 +2,28 @@ const express = require("express")
 const socket = require("socket.io")
 const http = require("http")
 const { Chess } = require("chess.js")
+const path = require("path")
+const { title } = require("process")
+
+const app = express() ;
+const server = http.createServer(app);
+const io = socket(server);
+
+const chess = new Chess() ;
+let players = {} ;
+let currentPlayer = "W" ;
+
+app.set("view engine","ejs") ;
+app.use(express.static(path.join(__dirname,"public"))) ;
+
+app.get("/",(req,res) => {
+    res.render("index" , { title : "Chess Game" });
+});
+
+
+
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+}); 
